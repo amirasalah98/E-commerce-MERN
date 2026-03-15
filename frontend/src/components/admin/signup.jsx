b7ea78321feee165d4f79e123df167b8f08564bb
Amira Salah <amira.salahhh98@gmail.com>
2026-03-15 03:53:27 +0200

    push edits

--- frontend/src/components/admin/signup.jsx
+++ frontend/src/components/admin/signup.jsx
@@ -21,7 +21,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signup`, {
+      await axios.post(`${import.meta.env.VITE_API_URL}/api/auth/signup`, {
         username,
         email,
         password
